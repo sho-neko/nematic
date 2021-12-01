544d2c8db25134bce80f9404d4f646a06d0d29fc
ShoNishino <nittundoraemon@outlook.com>
2021-12-01 17:17:32 +0900

    ok

--- lc.cpp
+++ lc.cpp
@@ -4,31 +4,26 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 #include <random>
+constexpr int FLOAT_MIN = 0;
+constexpr int FLOAT_MAX = 1;
 
-int main() {
-     // メルセンヌ・ツイスター法による擬似乱数生成器を、
-  // ハードウェア乱数をシードにして初期化
-  std::random_device seed_gen;
-  std::mt19937 engine(seed_gen());
+int main()
+{
+    std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_real_distribution<float> distr(FLOAT_MIN, FLOAT_MAX);
+    /*
+    for (int n = 0; n < 5; ++n)
+    {
+        cout << setprecision(6)
+             << distr(eng) << "\n";
+    }*/
 
-  // 一様実数分布
-  // [-1.0, 1.0)の値の範囲で、等確率に実数を生成する
-  std::uniform_real_distribution<> dist1(-1.0, 1.0);
-
- 
-
-  std::ofstream file("random.tsv");
-  for (size_t i = 0; i < 1000*1000; ++i) {
-    // 各分布法に基いて乱数を生成
-    double r1 = dist1(engine);
-
-    file << r1 <<  "\n";
-  }
-    std::uniform_real_distribution<double> get_rand_uni_real( 0.0, 1.0 );
     int N;
-    N=100;
-    double a[N+10][N+10];
-    rep(i,100){
-    //cout<<get_rand()<<endl;
+    N = 100;
+    double a[N + 10][N + 10];
+    rep(i, 100)
+    {
+        // cout<<get_rand()<<endl;
     }
 }
--- test.cpp
+++ test.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 #include <random>
 
-int main() {
-     // メルセンヌ・ツイスター法による擬似乱数生成器を、
+int main()
+{
+  // メルセンヌ・ツイスター法による擬似乱数生成器を、
   // ハードウェア乱数をシードにして初期化
   std::random_device seed_gen;
   std::mt19937 engine(seed_gen());
@@ -15,20 +16,20 @@ int main() {
   // [-1.0, 1.0)の値の範囲で、等確率に実数を生成する
   std::uniform_real_distribution<> dist1(-1.0, 1.0);
 
- 
-
   std::ofstream file("random.tsv");
-  for (size_t i = 0; i < 1000*1000; ++i) {
+  for (size_t i = 0; i < 1000 * 1000; ++i)
+  {
     // 各分布法に基いて乱数を生成
     double r1 = dist1(engine);
 
-    file << r1 <<  "\n";
+    file << r1 << "\n";
   }
-    std::uniform_real_distribution<double> get_rand_uni_real( 0.0, 1.0 );
-    int N;
-    N=100;
-    double a[N+10][N+10];
-    rep(i,100){
-    //cout<<get_rand()<<endl;
-    }
+  std::uniform_real_distribution<double> get_rand_uni_real(0.0, 1.0);
+  int N;
+  N = 100;
+  double a[N + 10][N + 10];
+  rep(i, 100)
+  {
+    // cout<<get_rand()<<endl;
+  }
 }
